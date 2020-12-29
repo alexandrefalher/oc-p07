9d618d256b498780e421a283788f9a97715bf9d6
Alexandre Falher <alexandre.falher.pro@gmail.com>
2020-12-29 22:56:11 +0100

    rename get_combations into get_combination_result. Satisfying version of the algorithm

--- ait/src/algo.py
+++ ait/src/algo.py
@@ -9,30 +9,24 @@ def main():
         Share("share-2", 9, 5),
         Share("share-3", 15, 6)
     ])
-    combination_result: float = get_combination(shares, ShareHolder(), 500)
+    combination_result: float = get_combination_result(shares, ShareHolder(), 500)
     print("--------- RESULT ----------")
     print(combination_result.combinations)
     print("Total cost: ", combination_result.total_cost)
     print("Total profit: ", combination_result.total_profit)
 
 
-def get_combination(shares: ShareHolder, combinations: ShareHolder, limit: float) -> CombinationResult:
+def get_combination_result(shares: ShareHolder, combinations: ShareHolder, limit: float) -> CombinationResult:
     if len(shares) > 0:
         shares_temp: ShareHolder = shares.copy()
-        result_1: CombinationResult = get_combination(shares_temp, combinations.copy().append(shares_temp.remove(0)), limit)
-        result_2: CombinationResult = get_combination(shares_temp, combinations, limit)
-        if result_1.total_profit > result_2.total_profit:
-            return result_1
-        else:
-            return result_2
+        result_1: CombinationResult = get_combination_result(shares_temp, combinations.copy().append(shares_temp.remove(0)), limit)
+        result_2: CombinationResult = get_combination_result(shares_temp, combinations, limit)
+        return result_1 if result_1.total_profit > result_2.total_profit else result_2
     elif len(shares) == 0:
         total_cost: int = combinations.calculate_total_cost()
         total_profit: float = combinations.calculate_final_value()
         print_step(combinations, total_cost, total_profit)
-        if total_cost <= limit:
-            return CombinationResult(combinations, total_cost, total_profit)
-        else:
-            return CombinationResult(combinations, total_cost, 0)
+        return CombinationResult(combinations, total_cost, total_profit) if total_cost <= limit else CombinationResult(combinations, total_cost, 0)
 
 
 def print_step(combinations: ShareHolder, total_cost_shares_combinations: int, total_profit_current_shares_combination: float) -> None:
